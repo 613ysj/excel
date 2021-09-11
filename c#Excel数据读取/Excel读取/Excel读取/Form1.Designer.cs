200beef9cfd50f7817fbb79ca23a313c4bc7f922
613ysj <1632810747@qq.com>
2021-09-11 10:08:12 +0800

    excel使用

---
+++ c#Excel数据读取/Excel读取/Excel读取/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Excel读取
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBoxRead = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(283, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "BtnRead";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBoxRead
+            // 
+            this.richTextBoxRead.Location = new System.Drawing.Point(28, 22);
+            this.richTextBoxRead.Name = "richTextBoxRead";
+            this.richTextBoxRead.Size = new System.Drawing.Size(194, 211);
+            this.richTextBoxRead.TabIndex = 2;
+            this.richTextBoxRead.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 276);
+            this.Controls.Add(this.richTextBoxRead);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBoxRead;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ c#Excel数据读取/Excel读取/Excel读取/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using ClosedXML.Excel;
+
+namespace Excel读取
+{
+    public partial class Form1 : Form
+    {
+        int row1 = 1;
+        int column1 = 0;
+        XLWorkbook g_wb = new XLWorkbook();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g_wb = new XLWorkbook(@"E:\c#Excel数据读取\练习.xlsx");
+            IXLWorksheet sheet = g_wb.Worksheet(1);
+            if (column1<8)
+            {
+                column1++;
+                richTextBoxRead.AppendText(sheet.Cell(row1, column1).GetString()+" ");
+            }
+            else
+            {
+                column1 = 0;
+                richTextBoxRead.AppendText("\n");
+                if (row1 < 4) row1++;
+                else row1 = 1;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            g_wb = new XLWorkbook(@"E:\c#Excel数据读取\练习.xlsx");
+            IXLWorksheet sheet = g_wb.Worksheet(1);
+            if (column1 < 8)
+            {
+                column1++;
+                richTextBoxRead.AppendText(sheet.Cell(row1, column1).GetString() + " ");
+            }
+            else
+            {
+                column1 = 0;
+                richTextBoxRead.AppendText("\n");
+                if (row1 < 4) row1++;
+                else row1 = 1;
+            }
+        }
+    }
+}
